ccc03baf51c17c5563fecfcbc1f1c69262d66f26
Alireza <alirezat775@gmail.com>
2023-09-16 12:29:39 +0300

    update sample app

--- app/src/main/java/io/nativeblocks/nativeblockkit/sample/MainActivity.kt
+++ app/src/main/java/io/nativeblocks/nativeblockkit/sample/MainActivity.kt
@@ -6,11 +6,12 @@ import androidx.activity.compose.setContent
 import io.nativeblocks.blockKit.NativeblocksBlockHelper
 import io.nativeblocks.core.api.NativeblocksManager
 import io.nativeblocks.core.api.NativeblocksProvider
+import io.nativeblocks.magicKit.NativeblocksMagicHelper
 
 private const val NATIVEBLOCKS_API_KEY =
     "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3Y2FiY2FjMS00MzI3LTRkMWQtOTVmMS1hOGMyYzc4MjE3MzciLCJvcmciOiI1MmZiNjIzOC1kY2FiLTQwYTYtODdhZC02ZGYyYzliYzVlZDgiLCJpYXQiOjE2OTIzODQ0MTIsImV4cCI6MTcyMzkyMDQxMn0.0hF-KSXtcqxMNcar1OOBs-H9gDTkAoC903TXVw6bGDg"
 
-private const val NATIVEBLOCKS_API_URL = "http://192.168.1.39:8585/graphql"
+private const val NATIVEBLOCKS_API_URL = "http://192.168.1.61:8585/graphql"
 
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +19,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         NativeblocksManager.initialize(this, NATIVEBLOCKS_API_URL, NATIVEBLOCKS_API_KEY)
-
         NativeblocksBlockHelper.provideBlocks()
-
+        NativeblocksMagicHelper.provideMagics(this)
         setContent {
             NativeblocksProvider()
         }
